0b7a0d582d31aa6ec589e634264db350c821ee91
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2023-09-15 17:32:59 +0200

    add online.pug

--- controllers/DuoGames.js
+++ controllers/DuoGames.js
@@ -1,84 +1,35 @@
 const DuoGame = require("../models/DuoGame");
 const Quiz = require("../models/Quiz");
-
+const startRound = require('../services/gameRound');
 
 let playersWaiting = [];
-const quiz = {
-  category: 'Computer Science',
-  question: 'what is the name of the heighest bulding in the world?',
-  options: [
-    {
-      option: 'Eivel Tower',
-      isCorrect : false
-    },
-    {
-      option:'Eivel Tower',
-      isCorrect: false
-    },
-    {
-      option:'Eivel Tower',
-      isCorrect: false
-    },
-    {
-      option:'khalifa Tower',
-      isCorrect: true
-    }
-  ]
-}
-
-const setTimer = (user, socket, timer, resolve) => {
-  if(timer < 0) {
-    return resolve();
-  }
-  socket.emit('timer', timer);
-  user.socket.emit('timer', timer);
-  console.log(timer);
-  timer--;
-  setTimeout(() => setTimer(user, socket, timer, resolve), 1000);
-}
-
-const startRound = async (user, socket, timer, numberOfRounds) => {
-  if(numberOfRounds < 1) {
-    return;
-  }
-  socket.emit('start round', quiz);
-  user.socket.emit('start round', quiz);
-  try {
-    await new Promise((resolve) => {
-      setTimer(user, socket, timer, resolve);
-    })
-    
-    timer = 30;
-    await startRound(user, socket, timer, --numberOfRounds);
-  } catch (err) {
-    console.error(err);
-  }
-}
 
 const startDuoGame = (socket) => {
-  socket.on('new game', async (userId) => {
+  socket.on('new game', async (userId, category) => {
     if(playersWaiting.length > 0) {
       const user = playersWaiting.shift();
-      // // Get 5 Random Quizzes
-      // const quizzes = await Quiz.aggregate([{ $sample: { size: 5 } }]);
-      // const quizzesId = quizzes.map(quiz => quiz._id);
-      // console.log(quizzes);
+      // Get 5 Random Quizzes
+      const randomQuizzes = await Quiz.aggregate([{ $sample: { size: 5 } }]);
+      let quizzes = [];
+      for(let i = 0; i < 5; i++) {
+        quizzes.push({ quizId: randomQuizzes[i]._id });
+      }
+      console.log(quizzes);
       
-      // // Create Game
-      // const game = new DuoGame({
-      //   firstUser: userId,
-      //   secondUser: user.userId,
-      //   firstUserScore: userId,
-      //   secondUserScore: user.userId,
-      //   quizzes: quizzesId
-      // })
-      // await game.save();
-      
-      socket.emit('start game');
-      user.socket.emit('start game');
+      // Create Game
+      const game = new DuoGame({
+        firstUser: userId,
+        secondUser: user.userId,
+        category,
+        quizzes: quizzes
+      })
+      await game.save();
+
+      socket.emit('start game', game);
+      user.socket.emit('start game', game);
       const timer = 30;
       const numberOfRounds = 5;
-      await startRound(user, socket, timer, numberOfRounds);
+      await startRound(user, socket, quizzes, timer, numberOfRounds);
     }
     playersWaiting.push({ userId, socket });
   })
--- public/js/duoGame.js
+++ public/js/duoGame.js
@@ -2,29 +2,48 @@
 const socket = io();
 
 const startGame = document.getElementById('start-game');
+let rightAnswerClicked = false;
 
 startGame.onclick = () => {
   const userId = localStorage.getItem('id');
-  socket.emit('new game', userId);
-  socket.on('start game', () => {
+  socket.emit('new game', userId, 'Computer Science');
+  socket.on('start game', (game) => {
     console.log('Game Started');
+    socket.on('start round', (quiz) => {
+      const quizDom = document.querySelector('.quiz');
+      const questionDom = document.querySelector('.question');
+      questionDom.textContent = quiz.question;
+      const options = document.querySelectorAll('.option');
+      const yourScore = document.querySelector('.your-score');
+      const oponentScore = document.querySelector('.oponent-score');
+      quizDom.id = quiz._id;
+      console.log(game)
+      yourScore.textContent = game.firstUserScore;
+      oponentScore.textContent = game.secondUserScore;
+      let i = 0;
+      options.forEach(option => {
+        option.id = quiz.options[i]._id;
+        option.textContent = quiz.options[i].option;
+        option.dataset.isCorrect = quiz.options[i].isCorrect;
+        option.addEventListener('click', () => {
+          console.log(option);
+          console.log(option.dataset.isCorrect);
+          if(!rightAnswerClicked) {
+            socket.emit('answer', game._id, option.id);
+          }
+          if(option.dataset.isCorrect === 'true') {
+            rightAnswerClicked = true;
+          }
+        })
+        i++;
+      });
+    });
+    socket.on('timer', (timer) => {
+      const timerDom = document.querySelector('.timer');
+      timerDom.textContent = timer;
+    });
   });
-  socket.on('start round', (quiz) => {
-    const quizDom = document.querySelector('.quiz');
-    const questionDom = document.querySelector('.question');
-    questionDom.textContent = quiz.question;
-    
-    for(let i = 0; i < 4; i++) {
-      const option = document.createElement('div');
-      option.className = 'option';
-      option.textContent = quiz.options[i].option;
-      quizDom.append(option);
-    }
-  });
-  socket.on('timer', (timer) => {
-    const timerDom = document.querySelector('.timer');
-    timerDom.textContent = timer;
-  });
+  
 }
 
 socket.on('connect', () => {
