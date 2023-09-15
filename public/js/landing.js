7bb04b8663589107612914adf5a00fea93c8712c
Sirine Sehabi <sirinesehabi@gmail.com>
2023-09-15 14:36:08 +0100

    update

---
+++ public/js/landing.js
@@ -0,0 +1,8 @@
+document.getElementById("login").addEventListener("click", function () {
+  // Redirect to the login page
+  window.location.href = "login.pug"; // Replace "login.html" with the actual URL of your login page
+});
+document.getElementById("sigup").addEventListener("click", function () {
+  // Redirect to the login page
+  window.location.href = "signUp.pug"; // Replace "login.html" with the actual URL of your login page
+});
--- routes/pages.js
+++ routes/pages.js
@@ -1,8 +1,8 @@
-const { dashboard, registerPage, loginPage } = require('../controllers/pages');
-const router = require('express').Router();
+const { dashboard, registerPage, loginPage } = require("../controllers/pages");
+const router = require("express").Router();
 
-router.route('/').get(dashboard);
-router.route('/register').get(registerPage);
-router.route('/login').get(loginPage);
-
-module.exports = router
+router.route("/").get(dashboard);
+router.route("/register").get(registerPage);
+router.route("/login").get(loginPage);
+router.route("/landing").get(landingPage);
+module.exports = router;
