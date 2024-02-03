0bc11c9dc616207d0fc9672fbac9e90c1b9d1bab
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-02-03 16:04:08 +0100

    Update Round Time Limit To 20s

--- controllers/DuoGames.js
+++ controllers/DuoGames.js
@@ -1,30 +1,33 @@
-const DuoGame = require("../models/DuoGame");
-const Quiz = require("../models/Quiz");
+const DuoGame = require('../models/DuoGame');
+const Quiz = require('../models/Quiz');
 const startRound = require('../services/gameRound');
 
 let playersWaiting = [];
 
 const startDuoGame = (socket) => {
   socket.on('new game', async (userId, category) => {
-    if(playersWaiting.length > 0) {
+    if (playersWaiting.length > 0) {
       const user = playersWaiting.shift();
       // Get 5 Random Quizzes
-      const randomQuizzes = await Quiz.aggregate([{ $match: { category } }, { $sample: { size: 5 } }]);
+      const randomQuizzes = await Quiz.aggregate([
+        { $match: { category } },
+        { $sample: { size: 5 } },
+      ]);
       let quizzes = [];
-      for(let i = 0; i < 5; i++) {
+      for (let i = 0; i < 5; i++) {
         quizzes.push({ quizId: randomQuizzes[i]._id });
       }
       console.log(quizzes);
-      
+
       // Create Game
       const game = new DuoGame({
         firstUser: userId,
         secondUser: user.userId,
         category,
-        quizzes: quizzes
-      })
+        quizzes: quizzes,
+      });
       await game.save();
-      
+
       // const users = {
       //   firstUser: game.firstUser,
       //   secondUser: game.secondUser,
@@ -32,15 +35,21 @@ const startDuoGame = (socket) => {
 
       socket.emit('start game', game);
       user.socket.emit('start game', game);
-      const timer = 10;
+      const timer = 20;
       const numberOfRounds = 5;
-      return await startRound(user, socket, quizzes, timer, numberOfRounds, game);
+      return await startRound(
+        user,
+        socket,
+        quizzes,
+        timer,
+        numberOfRounds,
+        game
+      );
     }
     playersWaiting.push({ userId, socket });
-  })
-}
-
+  });
+};
 
 module.exports = {
-  startDuoGame
-}
+  startDuoGame,
+};
